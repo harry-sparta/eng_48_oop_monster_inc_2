7db6934bc643c21b5d74df376038246cb5702b6b
harry-sparta <ka.hei.harry@gmail.com>
2019-12-06 22:07:00 +0000

    Commits (5) Run 1. updated with grade setting and for loop

--- run_1.py
+++ run_1.py
@@ -27,7 +27,7 @@ workshop_2 = Workshop(university.uni_name, 'Spooky 101')
 workshop_3 = Workshop(university.uni_name, 'Creepy 101')
 
 ### Running scenario
-print('---------------'+ 'WELCOME TO ' + university.uni_name.upper() + '-------------------')
+print('###############'+ ' WELCOME TO ' + university.uni_name.upper() + ' ###################')
 print('\n-------------------Workshops currently available-------------------')
 print(workshop_1.subject, '\n' + workshop_2.subject, '\n' + workshop_3.subject)   # Listing workshops available
 print('\n----Currently no student or staff are enrolled in any work shops----')
@@ -38,8 +38,19 @@ print('\n----------With student and staff enrolled into Scary 101-------------')
 workshop_1.add_student(monster_1.student_id)    # Enrolling monster_1 (student) into Scary 101
 workshop_1.add_student(monster_2.student_id)    # Enrolling monster_2 (student) into Scary 101
 workshop_1.add_staff(monster_4.staff_id)    # Enrolling monster_4 (staff) into Scary 101
-print('Student (ids) currently in', workshop_1.subject, ': ', workshop_1.enrollment_student)    # showing student list
-print('Staff (ids) currently in', workshop_1.subject, ': ', workshop_1.enrollment_staff)    # showing staff list
-print('\n----------With a lecture theatre added to the Scary 101--------------')
+print('Student (ids) currently in', workshop_1.subject, ':')    # showing student list
+for student_id in workshop_1.enrollment_student:
+    print(student_id)
+print('\nStaff (ids) currently in', workshop_1.subject, ':')    # showing staff list
+for staff_id in workshop_1.enrollment_staff:
+    print(staff_id)
+print('\n------------- With a lecture theatre added to Scary 101--------------')
 workshop_1.add_lecture_theatre(lecture_theatre_1.lecture_theatre_name)   # Adding a lecture theatre to Scary 101
-print('Lecture theatre for the workshop Scary 101 is in: ', workshop_1.lecture_theatre) # showing lecture theatre
+print('Lecture theatre for the workshop Scary 101 is in: ') # showing lecture theatre
+for theatre_name in workshop_1.lecture_theatre:
+    print(theatre_name)
+print('\n------------ Grading students enrolled on to Scary 101 ---------------')
+monster_1.set_grade('A')
+monster_2.set_grade('B')
+print('Inform monster_1 (student) with student_id', monster_1.student_id, 'its grade is', monster_1.see_grade())
+print('\n########################## THANK YOU #################################')
